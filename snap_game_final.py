38f41235e4c1fcdf5a3763a892f95dfa6ace6e2a
pineappleBottom <ibtisam.sheikh7@outlook.com>
2022-03-27 20:31:47 +0100

    code to make player shout snap at random/also fixed some typos

--- snap_game_final.py
+++ snap_game_final.py
@@ -5,59 +5,14 @@ import time
 ##requirement: Ask user how many playing card decks to play with?
 while True:
   try:
-    x = int(input("How many playing card decks: "))
+    x = int(input("How many playing card decks (maximum 1 only): "))
     if x == 1:
       break
     elif x > 1:
       print("Maximum 1 deck sorry") 
   except ValueError:
     print("Oops!  That was no valid number.  Try again...")
-
-## Create class Card, create instances of objects by assigning to suit and value
-## These are set to whatever we pass in when creating a card
-class Card(object):
-  """docstring for Card"""
-  def __init__(self, suit, value):
-    self.suit = suit
-    self.value = value
-
-  ## another method to show cards, takes self - will print out value and suit
-  def show(self):
-    print("{} of {}".format(self.value, self.suit))
-
-## Create class Deck
-class Deck(object):
-  def __init__(self):
-    ## initialise an attribute called cards
-    self.cards = []
-    #method to build deck, takes self, creating 52 cards
-    self.build()
-
-  ## method build() declaration, creating suits/cards 
-  ## 1-14 non-inclusive
-  def build(self):
-    for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-      #non-inclusive
-      for v in range(1, 14):
-        #print(v)
-        #print("{} of {}".format(v, s))
-        self.cards.append(Card(s, v))
-import random
-import time
-#import numpy as np
-import sys
-
-## 1) Requirement: Ask user how many playing card decks to play with?
-while True:
-  try:
-    x = int(input("How many playing card decks: "))
-    if x == 1:
-      break
-    elif x > 1:
-      print("Maximum 1 deck sorry") 
-  except ValueError:
-    print("Oops!  That was no valid number.  Try again...")
-
+    
 ## Create class Card, create instances of objects by assigning to suit and value
 ## These are set to whatever we pass in when creating a card
 class Card(object):
@@ -151,19 +106,32 @@ for i in range(1, 27):
   p2.draw(deck)
 
 count = 1
+## players yell snap at random
+a = ["Player 1:", "Player 2:"]
 while count <= 26:
     x = p1.playCard()
     y = p2.playCard()
     if x.value == y.value:
+        print("-----------------------------------------------------------")
         print("Player 1:") 
         x.show()
         print("Player 2:")
         y.show()
+
+        winner = random.choice(a)
+        print("\n", winner)
         print("Snap!\n")
+
+        if winner == "Player 1:":
+          print(winner)
+        elif winner == "Player 2:":
+          print(winner)
+        
         count+=1
         time.sleep(1)
         break
     else: 
+        print("-----------------------------------------------------------")
         print("Player 1:") 
         x.show()
         print("Player 2:") 
